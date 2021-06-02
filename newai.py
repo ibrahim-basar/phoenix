38ab87c7f8ecb1d2d8ae462172415d6ca53bd7db
ibrahim-basar <52051845+ibrahim-basar@users.noreply.github.com>
2021-06-02 17:17:20 +0300

    Update newai.py

--- newai.py
+++ newai.py
@@ -22,7 +22,7 @@ while True:
                       float(jsonData["tankA"]["y"]),
                       float(jsonData["tankA"]["r"]),
                       float(jsonData["tankA"]["can_fire"])]
-    data = requests.get(get_surl, headers=get_headers)
+    data = requests.get(get_surl, headers=get_headersiki)
     jsonData = json.loads(data.text)
 
     
